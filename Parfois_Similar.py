106f3c296a19d4311da9f55f7149752e2636740a
AJCFEP <up199401247@edu.fep.up.pt>
2025-12-04 21:07:15 +0000

    Update Parfois_Similar.py

--- Parfois_Similar.py
+++ Parfois_Similar.py
@@ -103,7 +103,7 @@ st.markdown(
             padding-top: 1.8rem;
         }
         .stApp [data-testid="stImage"] img {
-            margin-bottom: 5rem;
+            margin-bottom: 0rem;
         }
         h1, h2, h3 {
             margin-top: 0.2rem !important;
