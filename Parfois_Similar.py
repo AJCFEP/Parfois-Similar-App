73b7da451e8df647d073e0d15309f27c4e3012b8
AJCFEP <up199401247@edu.fep.up.pt>
2025-12-04 20:17:54 +0000

    Update Parfois_Similar.py

--- Parfois_Similar.py
+++ Parfois_Similar.py
@@ -290,7 +290,7 @@ st.markdown("<hr>", unsafe_allow_html=True)
 st.markdown(
     """
     <div style="font-size:32px; font-weight:600;
-                margin-top:1px; margin-bottom:4px;">
+                margin-top:1px; margin-bottom:1px;">
         Explore product similarities
     </div>
     """,
